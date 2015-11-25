50bfffbee725c4f018bc5c90ed9ad533b42d422c
99-ls1-ss <branpayne69@gmail.com>
2015-11-25 17:00:25 -0500

    Trying to create some logic on the home page.

--- Budget/Controllers/HomeController.cs
+++ Budget/Controllers/HomeController.cs
@@ -1,13 +1,34 @@
-﻿using System;
+﻿using Budget.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace Budget.Controllers {
     public class HomeController : Controller {
-        public ActionResult Index() {
-            return View();
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+
+        public ActionResult Index(int? id) {
+            HouseHold houseHold = db.HouseHoldData.Find(id);
+            ApplicationUser member = db.Users.Find(id);
+            //var user = UserManager.FindById(User.Identity.GetUserId());
+
+            if(User.Identity.IsAuthenticated == true) {
+                if(houseHold.Id == member.HouseHoldId) {
+                    return RedirectToAction("Index", "HouseHolds", new { id = houseHold.Id });
+                }
+                else {
+                    return View();
+                }
+                
+            }
+            else {
+                return View();
+            }
+            
         }
 
         public ActionResult About() {
